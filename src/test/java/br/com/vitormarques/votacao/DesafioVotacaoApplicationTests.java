72a3a58e24e6d2768f94b00ced3f9c281a16b39a
Jack Marques <pimentadesenvolvimento@gmail.com>
2026-09-04 10:56:34 -0300

    setup docker + testes

--- src/test/java/br/com/vitormarques/votacao/DesafioVotacaoApplicationTests.java
+++ src/test/java/br/com/vitormarques/votacao/DesafioVotacaoApplicationTests.java
@@ -2,12 +2,13 @@ package br.com.vitormarques.votacao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DesafioVotacaoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
